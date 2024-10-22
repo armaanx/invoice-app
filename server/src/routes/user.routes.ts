8c527241a684d5c5af065dc414e9399393ba7eb8
Armaan Mishra <armaanmishra48@gmail.com>
2024-10-23 04:27:26 +0530

    Add refreshAccessToken endpoint

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { verifyToken } from "../middleware/auth.middleware.ts";
 import {
   loginUser,
   logOutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.ts";
 import { Router } from "express";
@@ -12,5 +13,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyToken, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
