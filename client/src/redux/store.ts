af520dd8c1cf74b48c13bf56a77f70cffae1351d
Armaan Mishra <armaanmishra48@gmail.com>
2024-10-24 10:24:43 +0530

    Add products and pdf reducers to the store configuration

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import productsReducer from "./products/productSlice";
+import pdfReducer from "./pdf/pdfSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    products: productsReducer,
+    pdf: pdfReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
 
 export default store;
