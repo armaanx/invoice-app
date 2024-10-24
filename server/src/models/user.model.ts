27db4134ed1e3e9527d17a32c8a6ee44e3d27e60
Armaan Mishra <armaanmishra48@gmail.com>
2024-10-24 07:08:32 +0530

    Refactor user.model.ts to remove unused methods and properties

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,14 +1,10 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import mongoose, { Document, Model, Schema } from "mongoose";
 
 interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  refreshToken: string;
-  generateAccessToken(): string;
-  generateRefreshToken(): string;
   isValidPassword(password: string): Promise<boolean>;
 }
 
@@ -23,7 +19,6 @@ const userSchema = new Schema<IUser>(
       lowercase: true,
     },
     password: { type: String, required: [true, "Password is required"] },
-    refreshToken: { type: String },
   },
   { timestamps: true }
 );
@@ -41,29 +36,6 @@ userSchema.methods.isValidPassword = async function (
   password: string
 ) {
   return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function (this: IUser) {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      name: this.name,
-    },
-    process.env.ACCESS_TOKEN_SECRET!,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY! }
-  );
-};
-userSchema.methods.generateRefreshToken = function (this: IUser) {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      name: this.name,
-    },
-    process.env.REFRESH_TOKEN_SECRET!,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY! }
-  );
 };
 
 export const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
