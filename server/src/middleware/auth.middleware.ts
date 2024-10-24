1c3ddd907cbf907df6b4813ecf6bb901556285e9
Armaan Mishra <armaanmishra48@gmail.com>
2024-10-24 07:07:53 +0530

    Refactor auth.middleware.ts to use session-based authentication

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,44 +1,19 @@
-import { User } from "../models/user.model";
-import { asyncHandler } from "../utils/asyncHandler.ts";
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
 
-interface JwtPayload {
-  _id: string;
+declare module "express-session" {
+  interface Session {
+    userId?: string;
+  }
 }
 
-const verifyToken = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const token =
-        req.cookies?.accessToken || req.header("Authorization")?.split(" ")[1];
-      if (!token) {
-        return res.status(401).json({ message: "Access Token is required" });
-      }
-      const decodedToken = jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET!
-      ) as JwtPayload;
-
-      const user = await User.findById(decodedToken?._id).select(
-        "-password -refreshToken"
-      );
-
-      if (!user) {
-        return res.status(401).json({ message: "Invalid Access Token" });
-      }
-
-      //@ts-ignore
-      req.user = user;
-      next();
-    } catch (err) {
-      if (err instanceof Error) {
-        res.status(401).json({ message: err.message });
-      } else {
-        res.status(401).json({ message: "Something went wrong" });
-      }
-    }
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.session.userId) {
+    return next();
+  } else {
+    return res.status(401).json({ message: "Unauthorized" });
   }
-);
-
-export { verifyToken };
+};
