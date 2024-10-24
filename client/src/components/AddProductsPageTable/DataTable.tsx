9fdcbeb86a57e1df42dd5c9426ad0677fea7d89b
Armaan Mishra <armaanmishra48@gmail.com>
2024-10-25 02:14:42 +0530

    Refactor DataTable component to display "No Products" instead of "No results"

--- client/src/components/AddProductsPageTable/DataTable.tsx
+++ client/src/components/AddProductsPageTable/DataTable.tsx
@@ -78,7 +78,7 @@ export function DataTable<TData, TValue>({
           ) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
+                No Products.
               </TableCell>
             </TableRow>
           )}
